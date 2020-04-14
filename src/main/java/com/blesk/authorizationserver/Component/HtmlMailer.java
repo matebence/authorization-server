06833a882916c8e10c5e9ebad694d64793061784
Bence <m.bence05@gmail.com>
2020-04-14 23:05:03 +0200

    pridanie subor the htmlmailer

--- src/main/java/com/blesk/authorizationserver/Component/HtmlMailer.java
+++
@@ -1,8 +0,0 @@
-package com.blesk.authorizationserver.Component;
-
-import java.util.Map;
-
-public interface HtmlMailer {
-
-    String generateMailHtml(String htmlfile,  Map<String, Object> variables);
-}
