13320bea53e52f24de8d10a849add7bfd329a4ad
Bence <m.bence05@gmail.com>
2020-04-24 18:43:29 +0200

    travis ci bol pridany ako aj discoveryclient pre naming server

--- src/main/java/com/blesk/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/com/blesk/authorizationserver/AuthorizationServerApplication.java
@@ -2,8 +2,10 @@ package com.blesk.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableDiscoveryClient
 @EnableAuthorizationServer
 @SpringBootApplication
 public class AuthorizationServerApplication {
