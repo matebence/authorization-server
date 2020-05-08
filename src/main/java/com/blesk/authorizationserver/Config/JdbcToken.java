edd4a0548287bb405c2cb97f12d7616c6d647d48
Bence <m.bence05@gmail.com>
2020-05-08 17:29:34 +0200

    docker script na cakanie ostatnych containerov, bugfix custom jdbctoken triedy

--- src/main/java/com/blesk/authorizationserver/Config/JdbcToken.java
+++ src/main/java/com/blesk/authorizationserver/Config/JdbcToken.java
@@ -2,24 +2,40 @@ package com.blesk.authorizationserver.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.util.Assert;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Configuration
 public class JdbcToken extends JdbcTokenStore {
 
+    private final JdbcTemplate jdbcTemplate;
+
     public JdbcToken(DataSource dataSource) {
         super(dataSource);
+        Assert.notNull(dataSource, "DataSource required");
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
     public OAuth2AccessToken readAccessToken(String tokenValue) {
-        OAuth2AccessToken oAuth2AccessToken = null;
+        OAuth2AccessToken accessToken = null;
+
         try {
-            oAuth2AccessToken = super.readAccessToken(tokenValue);
-        } catch (Exception ignore) {}
-        return oAuth2AccessToken;
+            accessToken = (OAuth2AccessToken)this.jdbcTemplate.queryForObject("select token_id, token from oauth_access_token where token_id = ?", new RowMapper<OAuth2AccessToken>() {
+                public OAuth2AccessToken mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    return JdbcToken.this.deserializeAccessToken(rs.getBytes(2));
+                }
+            }, new Object[]{this.extractTokenKey(tokenValue)});
+        } catch (EmptyResultDataAccessException ignore) {}
+        catch (IllegalArgumentException ignore) {this.removeAccessToken(tokenValue);}
+
+        return accessToken;
     }
 }
