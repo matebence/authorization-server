94679b6ac77251118c85cff09b9da329fa2c047f
Bence <m.bence05@gmail.com>
2020-06-20 18:51:55 +0200

    premiestnenie isDeleted z AccountPreferences do Preferences

--- src/main/java/com/blesk/authorizationserver/Model/AccountPreferences.java
+++ src/main/java/com/blesk/authorizationserver/Model/AccountPreferences.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, scope = AccountPreferences.class)
 public class AccountPreferences implements Serializable {
@@ -21,13 +20,19 @@ public class AccountPreferences implements Serializable {
 
     private int value;
 
-    private Boolean isDeleted = false;
+    public AccountPreferences(boolean isSet, String content, int value, Accounts accounts, Preferences preferences) {
+        this.isSet = isSet;
+        this.content = content;
+        this.value = value;
+        this.accounts = accounts;
+        this.preferences = preferences;
+    }
 
-    private Timestamp createdAt;
-
-    private Timestamp updatedAt;
-
-    private Timestamp deletedAt;
+    public AccountPreferences(boolean isSet, String content, int value) {
+        this.isSet = isSet;
+        this.content = content;
+        this.value = value;
+    }
 
     public AccountPreferences(Accounts accounts, Preferences preferences) {
         this.accounts = accounts;
@@ -39,22 +44,6 @@ public class AccountPreferences implements Serializable {
     }
 
     public AccountPreferences(Preferences preferences) {
-        this.preferences = preferences;
-    }
-
-    public AccountPreferences(boolean isSet, String content, int value, Boolean isDeleted) {
-        this.isSet = isSet;
-        this.content = content;
-        this.value = value;
-        this.isDeleted = isDeleted;
-    }
-
-    public AccountPreferences(boolean isSet, String content, int value, Boolean isDeleted, Accounts accounts, Preferences preferences) {
-        this.isSet = isSet;
-        this.content = content;
-        this.value = value;
-        this.isDeleted = isDeleted;
-        this.accounts = accounts;
         this.preferences = preferences;
     }
 
@@ -107,37 +96,5 @@ public class AccountPreferences implements Serializable {
 
     public void setValue(int value) {
         this.value = value;
-    }
-
-    public Boolean getDeleted() {
-        return this.isDeleted;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        this.isDeleted = deleted;
-    }
-
-    public Timestamp getCreatedAt() {
-        return this.createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Timestamp getUpdatedAt() {
-        return this.updatedAt;
-    }
-
-    public void setUpdatedAt(Timestamp updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public Timestamp getDeletedAt() {
-        return this.deletedAt;
-    }
-
-    public void setDeletedAt(Timestamp deletedAt) {
-        this.deletedAt = deletedAt;
     }
 }
